fc7a3dd6740e697165459a325a734328674f0fa5
yashlamba <lambayash958@gmail.com>
2026-07-14 16:44:00 +0530

    ok

--- to-do-list.py
+++ to-do-list.py
@@ -1,3 +1,4 @@
+from asyncio import tasks
 import json
 
 filename = "todo_list.json"
@@ -16,20 +17,35 @@ def save_tasks(tasks):
     except:
         print("Error saving tasks to file.")
 
-def add_tasks():
-    pass
+def add_tasks(tasks):
+    description = input("Enter the task description: ").strip()
+    if description:
+        tasks["tasks"].append({"description": description, "completed": False})
+        save_tasks(tasks)
+        print("Task added successfully.")
+    else:
+        print("Description not given.") 
 
-def mark_tasks_complete():
-    pass   
+def mark_tasks_complete(tasks):
+    
+     
 
 def delete_tasks():
     pass
 
-def display_tasks():
-    pass
+def display_tasks(tasks):
+    task_list = tasks["tasks"]
+    if len(task_list) == 0:
+        print("No tasks present.")
+    else:
+        print("Your tasks:")
+        for idx, task in enumerate(task_list):
+            status = "Completed" if task["completed"] else "Incomplete"
+            print(f"{idx + 1}. {task['description']} - {status}")
 
 def main():
     tasks = load_tasks()
+
     print("Welcome to your To-Do List!")
     print('''1. Add tasks
 2. Mark Tasks as Complete
@@ -41,7 +57,7 @@ def main():
         choice = input("Enter your choice (1-6): ")
         if choice == '1':
             print("Add tasks")
-            add_tasks()
+            add_tasks(tasks)
         elif choice == '2':
             print("Mark tasks as complete")
             mark_tasks_complete()
@@ -49,8 +65,8 @@ def main():
             print("Delete tasks")
             delete_tasks()
         elif choice == '4':
-            print("Display tasks")
-            display_tasks()
+            print()
+            display_tasks(tasks)
         elif choice == '5':
             print("Save tasks")
             save_tasks(tasks)
